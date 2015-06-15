f6653af21afb976007efc36a56ce5e60fcc6c724
Christopher N. Hesse <raymanfx@gmail.com>
2015-06-15 15:49:46 +0200

    r5: Fix config paths
    Change-Id: Ic9dc798c3484d8b76a431b090745b5d2bd32eb81

--- device.mk
+++ device.mk
@@ -66,7 +66,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-    $(LOCAL_PATH)/mixer_paths.xml:/system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/configs/mixer_paths.xml:/system/etc/mixer_paths.xml
 
 # Thermal-engine
 PRODUCT_COPY_FILES += \
@@ -83,8 +83,8 @@ PRODUCT_COPY_FILES += \
 
 # Media configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
