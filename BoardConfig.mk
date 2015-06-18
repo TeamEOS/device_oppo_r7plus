79d6f5c2ef1734da18d629cd619563a3c83b2ba3
Christopher N. Hesse <raymanfx@gmail.com>
2015-06-18 08:12:26 +0200

    r5: Update mkbootimg args
    Change-Id: I002b808fbad6916af5abf320071da1add89a10ef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,9 +35,9 @@ TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 # Kernel
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 boot_cpus=0,4,5,6,7 androidboot.hardware=qcom androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci sched_enable_hmp=1
-BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
+BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 TARGET_KERNEL_CONFIG := eos_r5_defconfig
 TARGET_KERNEL_SOURCE := kernel/oppo/r5
