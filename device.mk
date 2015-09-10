2db555b94efc3e37350cb802e1d3c30714fd2281
Christopher N. Hesse <raymanfx@gmail.com>
2015-09-10 13:38:52 +0200

    r7plus: Add audio_platform_info XML
    Credits for figuring out the acdb ID's go to Rashed Abdel-Tawab.
    
    Change-Id: Ib5bdd8f4ac56fbaa68c0a239ed1e7d6fee4b31ca

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio_platform_info.xml:/system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/mixer_paths_15018.xml:/system/etc/mixer_paths_mtp.xml \
     $(LOCAL_PATH)/configs/aanc_tuning_mixer.txt:/system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/configs/sound_trigger_mixer_paths.xml:/system/etc/sound_trigger_mixer_paths.xml
